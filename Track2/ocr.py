6000c5a8d04c49c5e26ffb8ee577f959f48889a5
Priya Singh <159720938+priyasingh2907@users.noreply.github.com>
2026-07-09 20:36:02 +0530

    Add files via upload

---
+++ Track2/ocr.py
@@ -0,0 +1,31 @@
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
+
+processor = TrOCRProcessor.from_pretrained(
+    "microsoft/trocr-base-handwritten"
+)
+
+model = VisionEncoderDecoderModel.from_pretrained(
+    "microsoft/trocr-base-handwritten"
+)
+
+
+def recognize_code(image_path):
+    image = Image.open(image_path).convert("RGB")
+
+    pixel_values = processor(
+        images=image,
+        return_tensors="pt"
+    ).pixel_values
+
+    generated_ids = model.generate(
+        pixel_values,
+        max_new_tokens=150
+    )
+
+    text = processor.batch_decode(
+        generated_ids,
+        skip_special_tokens=True
+    )[0]
+
+    return text
